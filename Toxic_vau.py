955d575dc298553dbde9db3d036b35b1c2a2e60b
Ariyan-404 <99079519+TC404-Cyber@users.noreply.github.com>
2023-11-05 03:45:57 +0600

    Create Toxic_vau.py

---
+++ Toxic_vau.py
@@ -0,0 +1,11 @@
+import os
+import platform
+os.system('clear')
+os.system("git pull")
+b = platform.architecture()[0]
+if b == '64bit':
+    __import__("Toxic404").Toxic()
+    
+elif b == '32bit':
+    print('wait for 33bit')
+ 
