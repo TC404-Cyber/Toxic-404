f43febce9e265d1ec53a21860932b55ea6929e99
Ariyan-404 <99079519+TC404-Cyber@users.noreply.github.com>
2023-11-05 21:53:52 +0600

    Update Toxic_vau.py

--- Toxic_vau.py
+++ Toxic_vau.py
@@ -1,11 +1,2 @@
-import os
-import platform
-os.system('clear')
-os.system("git pull")
-b = platform.architecture()[0]
-if b == '64bit':
-    __import__("TRXX").Ariyan()
-    
-elif b == '32bit':
-    print('wait for 33bit')
- 
+import TRXX
+TRXX.Toxic()
